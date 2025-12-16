4fa3914ea477fd885d14a252a7cf0ba7f37ae983
Brandon <lawrebx@gmail.com>
2025-12-16 11:44:41 -0500

    day 8 submission

--- 2025/Day8/Day8.py
+++ 2025/Day8/Day8.py
@@ -1,74 +1,48 @@
-input = []
+junctions = []
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        x, y, z = (int(k) for k in line.split(','))
+        junctions.append((x, y, z))
 
-with open('input_test.txt', "r", encoding="utf-8") as file:
-    for count, line in enumerate(file, start=1):
-        sline = line.strip().split(",")
-        sline = [int(i) for i in sline]
-        input.append(sline)
+def dist2(j1, j2):
+    return sum((j1[k] - j2[k])**2 for k in range(3))
 
-def calc_distance(input1:list,input2:list):
+union_map = {}
+class Union:
+    def __init__(self, members):
+        self.members = members
+        for m in members:
+            union_map[m] = self
+    def union(self, other):
+        new_members = self.members | other.members
+        return Union(new_members)
 
-    distance = ((input1[0]-input2[0])**2 + (input1[1]-input2[1])**2 + (input1[2]-input2[2])**2)**0.5
+for p in junctions:
+    Union({p})
 
-    return distance
+all_pairs = []
+for i, p in enumerate(junctions):
+    for j in range(i+1, len(junctions)):
+        q = junctions[j]
+        all_pairs.append((p, q))
+all_pairs.sort(key=lambda pair: dist2(pair[0], pair[1]))
 
-min_coords = (0,0)
-combo_list = []
+part_1_cap = 1000
 
-min_dist = None
+for i, (p, q) in enumerate(all_pairs):
+    # part 1
+    if i == (part_1_cap):
+        unions = set()
+        for p in junctions:
+            unions.add(union_map[p])
+        unions = list(sorted(unions, key=lambda u: len(u.members), reverse=True))
+        print(len(unions[0].members) * len(unions[1].members) * len(unions[2].members))
 
-
-for coord in input:
-    min_dist = None
-    for check_coord in input:
-        if coord != check_coord:
-            dist = calc_distance(coord, check_coord)
-            if min_dist == None:
-                min_dist = dist
-                min_coords = (coord, check_coord, dist)
-            elif min_dist > dist:
-                min_dist = dist
-                min_coords = (coord, check_coord, dist)   
-
-    combo_list.append(min_coords)
-
-if min_dist != None:
-    print(f'Min Distance: {min_dist//1} | Min Coords {min_coords}')
-
-print('-------------------------')
-sorted_list = sorted(combo_list, key=lambda combo_list: combo_list[2])
-
-circuits = {}
-circuit_count = 0
-
-for combo_d in sorted_list:
-    combo = combo_d[0], combo_d[1]
-
-    combo = list(combo)
-
-    if circuits == {}:
-        circuits[circuit_count] = combo
-        print(circuits)
+    if union_map[p] == union_map[q]:
         continue
-
-    existing_circuit = 0
-
-    for key in circuits:
-        if combo[0] in circuits[key]:
-            circuits[key].extend(combo)
-            continue
-        elif combo[1] in circuits[key]:
-            circuits[key].extend(combo)
-            continue   
-
-    circuit_count = circuit_count + 1
-    circuits[circuit_count] = combo
+    u = union_map[p].union(union_map[q])
     
-    print(circuits)
-
-sorted_circuits = dict(sorted(circuits.items(), key=lambda item: len(item[1]), reverse=True))
-
-for c in sorted_circuits:
-    print(len(circuits[c]))
-
-
+    # part 2
+    if len(u.members) == len(junctions):
+        print(p[0]*q[0])
+        break
